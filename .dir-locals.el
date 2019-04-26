d1230e43bc37d3b1453b3b1d8ca70e89b3b58d71
zaijian <czy175898@alibaba-inc.com>
2019-04-26 22:13:12 +0800

    commit by elisp

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-html-postamble . nil)
+  (org-html-preamble-format
+   ("en" "<p class=\"author\">Author: %a</p>\n<p class=\"date\">Updated: %C</p>"))))
